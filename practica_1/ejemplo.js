98e334dd4ada2787292abe33883a25c2cf3c3452
L17_MAX <luceropichogarcia7@gmail.com>
2024-10-24 12:16:39 -0500

    mi primer commit

---
+++ practica_1/ejemplo.js
@@ -0,0 +1,38 @@
+ // variables
+ // var
+//  var a = 1
+//  var a = 2
+// console.log(a);
+ // let 
+//  let a = 1
+//  //const
+//  const pi = 3.1416
+//  const meses = 12
+//  var nombre = "juan"
+//  var edad = 24
+console.log("")
+console.log("---------------------------------------------------")
+console.log("")
+console.log("ÁREA DEL CUADRADO")
+let lado = 5
+AreaC = lado * lado ;
+console.log("El area del cuadrado es: " + AreaC);
+console.log("")
+console.log("---------------------------------------------------")
+console.log("")
+console.log("ÁREA DEL TRIANGULO")
+let base = 4
+let altura = 6
+AreaT = (base * altura)/2
+console.log("El area del triangulo es: " + AreaT);
+console.log("")
+console.log("---------------------------------------------------")
+console.log("")
+console.log("ÁREA DEL RECTANGULO")
+let base1 = 10
+let altura1 = 5
+AreaR = (base * altura)
+console.log("El area del rectangulo es: " + AreaR);
+console.log("")
+console.log("---------------------------------------------------")
+console.log("")
